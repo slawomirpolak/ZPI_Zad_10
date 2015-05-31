060a16ceefd15024c6bb03d23906f3c5b39ab54b
slawomirpolak <slawomirpolak93@gmail.com>
2015-05-31 23:03:09 +0200

    Dodanie pierwszego testu

---
+++ Problem_10_TDD_Test.java
@@ -0,0 +1,30 @@
+package problem_10;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Slawek
+ */
+public class Problem_10Test {
+   
+  
+ 
+
+    @Test
+    public void testIsPrime() {
+        System.out.println("Testowanie metody isPrime");
+        int n = 7;
+        Problem_10 instance = new Problem_10();
+        boolean expResult = true;
+        boolean result = instance.isPrime(n);
+        assertEquals(expResult, result);
+       
+    }
+    
+}
